64afd8ee038140ca26ab3ecc8c1ba5d2b20f61d4
George Trubachef <trubacheff_91@mail.ru>
2021-09-10 23:59:58 +0300

    style: настроить адаптивность

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: false,
-    allowTouchMove: false,
+const swiper = new Swiper('.team__slider', {
+    wrapperClass: 'team__slider-items',
+    slideClass: 'team__slider-item',
     slidesPerView: 3,
     spaceBetween: 80,
-    autoplay: {
-        delay: 3000
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        768: {
+            spaceBetween: 28,
+            slidesPerView: 2
+        },
+        1050: {
+            slidesPerView: 2,
+            spaceBetween: 28
+        },
+        1250: {
+            slidesPerView: 3,
+            spaceBetween: 80
+        },
+        1440: {
+            slidesPerView: 3,
+            spaceBetween: 80
+
+        }
     },
-    allowTouchMove: false,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        bulletElement: 'button',
+        bulletClass: 'team__dot',
+        currentClass: 'team__dot_active',
+        clickable: true,
+        bulletActiveClass: 'team__dot_active',
+    }
+});
+
+const mySwier = new Swiper('.game__slider', {
+    wrapperClass: 'game__slider-items',
+    slideClass: 'game__slider-item',
+    slidesPerView: 3,
+    spaceBetween: 80,
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        768: {
+            spaceBetween: 28,
+            slidesPerView: 2
+        },
+        1050: {
+            slidesPerView: 2,
+            spaceBetween: 28
+        },
+        1250: {
+            slidesPerView: 3,
+            spaceBetween: 80
+        },
+        1440: {
+            slidesPerView: 3,
+            spaceBetween: 80
+        }
+    },
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
